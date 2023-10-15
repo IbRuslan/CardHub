ea42fef9de2861831daf4207512e9c95e61d1040
hellpains <rustam2004sadulaev@mail.ru>
2023-10-15 23:11:36 +0300

    completed the input

--- src/components/ui/text-field/textField.stories.ts
+++ src/components/ui/text-field/textField.stories.ts
@@ -17,6 +17,37 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
+    placeholder: 'Input Primary',
+  },
+}
+
+export const Invalid: Story = {
+  args: {
+    error: 'Error!',
+    placeholder: 'Input Invalid',
+  },
+}
+
+export const Password: Story = {
+  args: {
+    password: true,
+    placeholder: 'Input Password',
+  },
+}
+
+export const Search: Story = {
+  args: {
+    disabled: true,
     placeholder: 'Input',
+    search: true,
+    value: 'Input Search',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    placeholder: 'Input',
+    value: 'Input Disabled',
   },
 }
