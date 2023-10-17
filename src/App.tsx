00492409a9da79a5e0e3b9dc6e8e2e636725d24f
IbRuslan <ribragimov2003@gmail.com>
2023-10-17 18:15:44 +0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import { Avatar, DropDownItem, DropDownMenu } from '@/components/ui'
-import { Button } from '@/components/ui/button'
-import { ExitIcon, HomeIcon } from '@radix-ui/react-icons'
+import { Header } from '@/components/ui'
 
 export function App() {
   return (
     <div>
-      <Button>hello</Button>
-      <DropDownMenu trigger={<HomeIcon />} variant={'profiledrop'}>
-        <DropDownItem
-          el={{ email: 'ribragimov2003@gmail.com', icon: <HomeIcon />, title: 'Ivan' }}
-          onSelect={() => {
-            console.log('profile')
-          }}
-        />
-        <DropDownItem
-          el={{ icon: <HomeIcon />, title: 'Ivan' }}
-          onSelect={() => {
-            console.log('profile')
-          }}
-        />
-        <DropDownItem
-          el={{ icon: <ExitIcon />, title: 'Log out' }}
-          onSelect={() => {
-            console.log('log out')
-          }}
-        />
-      </DropDownMenu>
-      <Avatar />
+      <Header />
     </div>
   )
 }
