ba2695e653fdd3a00cc3fd8069beee2c420aaa34
IbRuslan <ribragimov2003@gmail.com>
2023-10-25 22:22:37 +0500

    add new password form

---
+++ src/components/auth/new-password/new-password.tsx
@@ -0,0 +1,51 @@
+import { useForm } from 'react-hook-form'
+
+import { ControlledTextField } from '@/components/controlled'
+import { Button, Card, Typography } from '@/components/ui'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import s from './new-password.module.scss'
+
+const newPasswordSchema = z.object({
+  password: z.string().min(3, 'Too short password'),
+})
+
+type FormValues = z.infer<typeof newPasswordSchema>
+
+export const NewPassword = () => {
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: { password: '' },
+    mode: 'onSubmit',
+    resolver: zodResolver(newPasswordSchema),
+  })
+
+  const Submit = (data: FormValues) => {
+    console.log(data)
+  }
+
+  return (
+    <Card className={s.wrapper}>
+      <Typography as={'h2'} className={s.title} variant={'large'}>
+        Create new password
+      </Typography>
+      <form onSubmit={handleSubmit(Submit)}>
+        <div className={s.textfields}>
+          <ControlledTextField
+            control={control}
+            label={'Password'}
+            name={'password'}
+            password
+            placeholder={'Password'}
+          />
+        </div>
+        <Typography className={s.description} variant={'body2'}>
+          Create new password and we will send you further instructions to email
+        </Typography>
+        <Button className={s.button} fullWidth type={'submit'}>
+          Create new password
+        </Button>
+      </form>
+    </Card>
+  )
+}
