eead4ebcba0e96d8526b0f0f8c08459f6b97f9b0
IbRuslan <ribragimov2003@gmail.com>
2023-10-30 19:45:07 +0500

    add packs page

--- src/pages/packs/packs.tsx
+++ src/pages/packs/packs.tsx
@@ -10,8 +10,22 @@ import {
   TextField,
   Typography,
 } from '@/components/ui'
-import { useGetDecksQuery } from '@/services/base-api.ts'
-import { useAppDispatch, useAppSelector } from '@/services/store.ts'
+import {
+  selectDecksCurrentPage,
+  selectDecksCurrentTab,
+  selectDecksMaxCards,
+  selectDecksMinCards,
+  selectDecksSearch,
+} from '@/services/packs/packs.selectors'
+import {
+  useCreateDeckMutation,
+  useDeleteDeckMutation,
+  useGetDecksQuery,
+  useUpdateDeckMutation,
+} from '@/services/packs/packs.service'
+import { decksSlice } from '@/services/packs/packs.slice'
+import { Tab } from '@/services/packs/packs.types'
+import { useAppDispatch, useAppSelector } from '@/services/store'
 
 export const Packs = () => {
   const [showCreateModal, setShowCreateModal] = useState(false)
@@ -20,6 +34,27 @@ export const Packs = () => {
 
   const dispatch = useAppDispatch()
   const currentPage = useAppSelector(selectDecksCurrentPage)
+  const currentTab = useAppSelector(selectDecksCurrentTab)
+  const minCards = useAppSelector(selectDecksMinCards)
+  const maxCards = useAppSelector(selectDecksMaxCards)
+  const search = useAppSelector(selectDecksSearch)
+  const setCurrentPage = (page: number) => dispatch(decksSlice.actions.setCurrentPage(page))
+  const setMinCards = (minCards: number) => dispatch(decksSlice.actions.setMinCards(minCards))
+  const setMaxCards = (maxCards: number) => dispatch(decksSlice.actions.setMaxCards(maxCards))
+  const setSearch = (search: string) => dispatch(decksSlice.actions.setSearch(search))
+  const setCurrentTab = (tab: Tab) => dispatch(decksSlice.actions.setCurrentTab(tab))
+
+  const resetFilters = () => {
+    dispatch(decksSlice.actions.resetFilters())
+    setRangeValue([0, decks?.maxCardsCount || undefined])
+  }
+
+  const [rangeValue, setRangeValue] = useState([minCards, maxCards])
+
+  const handleSliderCommitted = (value: number[]) => {
+    setMinCards(value[0])
+    setMaxCards(value[1])
+  }
 
   const currentUserId = 'f2be95b9-4d07-4751-a775-bd612fc9553a'
   const authorId = currentTab === 'my' ? currentUserId : undefined
@@ -32,8 +67,24 @@ export const Packs = () => {
     name: search,
   })
 
+  const showConfirmDeleteModal = !!deckToDeleteId
+  const deckToDeleteName = decks?.items?.find(deck => deck.id === deckToDeleteId)?.name
+
+  const deckToEdit = decks?.items?.find(deck => deck.id === deckToEditId)
+
+  const [createDeck] = useCreateDeckMutation()
+  const [deleteDeck] = useDeleteDeckMutation()
+  const [updateDeck] = useUpdateDeckMutation()
+  const openCreateModal = () => setShowCreateModal(true)
+
   const value = 5
-  const changePageHandler = (page: number, value: number) => {}
+  const changePageHandler = (page: number, value: number) => {
+    setCurrentPage(page)
+  }
+
+  if (!decks) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -48,19 +99,30 @@ export const Packs = () => {
         </div>
       </div>
       <div>
-        <TextField></TextField>
-        <TabSwitcher defaultValue={'tab2'} onChange={() => {}} title={'Show packs cards'}>
-          <TabTrigger title={'my'} value={'tab1'} />
-          <TabTrigger title={'all'} value={'tab2'} />
+        <TextField
+          onValueChange={setSearch}
+          placeholder={'Search'}
+          search
+          value={search}
+        ></TextField>
+        <TabSwitcher
+          defaultValue={currentTab}
+          onChange={value => setCurrentTab(value as Tab)}
+          title={'Show packs cards'}
+        >
+          <TabTrigger title={'My packs'} value={'my'} />
+          <TabTrigger title={'All packs'} value={'all'} />
         </TabSwitcher>
         <SuperSlider
-          max={50}
+          max={decks?.maxCardsCount || 0}
           min={0}
-          onValueChange={() => {}}
-          onValueCommit={() => {}}
-          value={[]}
+          onValueChange={setRangeValue}
+          onValueCommit={handleSliderCommitted}
+          value={rangeValue}
         />
-        <Button variant={'secondary'}>Clear Filter</Button>
+        <Button onClick={resetFilters} variant={'secondary'}>
+          Clear Filter
+        </Button>
       </div>
       <PacksTable
         currentUserId={currentUserId}
--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,8 @@ import {
   createBrowserRouter,
 } from 'react-router-dom'
 
+import { Packs } from '@/pages'
+
 const publicRoutes: RouteObject[] = [
   {
     element: <div>login</div>,
@@ -15,8 +17,12 @@ const publicRoutes: RouteObject[] = [
 
 const privateRoutes: RouteObject[] = [
   {
-    element: <div>hello</div>,
+    element: <Packs />,
     path: '/',
+  },
+  {
+    element: <div>not work for now</div>,
+    path: '/decks/:deckId',
   },
 ]
 
